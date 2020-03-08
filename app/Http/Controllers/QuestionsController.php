7e6a65eda2f3fd55f8ad4c5c9a25033d3dd8a4a5
Joey <jclustre@gmail.com>
2020-03-08 12:03:26 -0700

    Add author info and creation date in question item

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -14,9 +14,20 @@ class QuestionsController extends Controller
      */
     public function index()
     {
-        $questions = Question::latest()->paginate(5);
+        $questions = Question::with('user')->latest()->paginate(5);
         return view('questions.index', compact('questions'));
     }
+
+/**
+* the following is used for debugging
+**/  
+    // public function index()
+    // {
+    //     \DB::enableQueryLog();
+    //     $questions = Question::with('user')->latest()->paginate(5);
+    //     view('questions.index', compact('questions'))->render();
+    //     dd(\DB::getQueryLog());
+    // }
 
     /**
      * Show the form for creating a new resource.
